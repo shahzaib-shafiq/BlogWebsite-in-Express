5518e221f10a60460898a6a26465876847b2f107
Android <shafiqshahzaib@gmail.com>
2023-07-10 14:46:51 +0500

    Final

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // //jshint esversion:6
 
 // const express = require("express");
@@ -87,6 +88,16 @@ const _ = require('lodash');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare...";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque...";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices...";
+=======
+//es6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var _ = require('lodash');
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+>>>>>>> Stashed changes
 
 const app = express();
 const Posts = [];
@@ -95,9 +106,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+<<<<<<< Updated upstream
 app.get("/", function(req, res) {
   res.render('home', { homeStarting: homeStartingContent, Posts: Posts });
 });
+=======
+app.get("/",function(req,res){ 
+
+  res.render('home', {homeStarting:homeStartingContent,Posts:Posts});    
+  
+})
+>>>>>>> Stashed changes
 
 app.get("/about", function(req, res) {
   res.render("about", { about: aboutContent });
@@ -120,8 +139,16 @@ app.post("/compose", function(req, res) {
   };
 
   Posts.push(ComposeArea);
+<<<<<<< Updated upstream
   res.redirect("/");
 });
+=======
+  res.redirect("/")
+
+
+  })
+  
+>>>>>>> Stashed changes
 
 app.get("/posts/:postName", function(req, res) {
   const RequestedTitle = _.lowerCase(req.params.postName);
@@ -130,13 +157,25 @@ app.get("/posts/:postName", function(req, res) {
     const storedTitle = _.lowerCase(post.Title);
     if (storedTitle === RequestedTitle) {
       res.render("post", {
+<<<<<<< Updated upstream
         title: post.Title,
         content: post.Text
+=======
+        Title:post.Title,
+        Text:post.Text
+>>>>>>> Stashed changes
       });
     }
   });
 });
 
+<<<<<<< Updated upstream
 app.listen(3000, function() {
+=======
+
+app.listen(3000, function() 
+{
+>>>>>>> Stashed changes
   console.log("Server started on port 3000");
 });
+
